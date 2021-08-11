70f2e12a7adf00d89b3d25c403d16fa675f7e0d3
Fabian Nawratil <fnawratil@users.noreply.github.com>
2021-08-11 09:53:23 +0200

    Implementation of RegisterParser (#2)
    * Implement RegisterParser
    
    * Fix array parsing
    
    * Fix terminated string parsing
    
    * Make ParseResult public
    
    * Make magic values const
    
    * Minimal comments
    
    * Trigger updated CodeQL

---
+++ Jacdac.Tests/RegisterParserTests.cs
@@ -0,0 +1,142 @@
+using System;
+using Xunit;
+
+namespace Jacdac.Tests
+{
+    public class RegisterParserTests
+    {
+        [Fact]
+        public void ParseSimpleBuffer()
+        {
+            var buffer = new byte[] { 0x1, 0x2, 0x3, 0x4 };
+            var format = "b";
+            var parsed = RegisterParser.ParseBuffer(format, buffer);
+
+            Assert.True(parsed.IsSingleValue);
+            Assert.IsType<byte[]>(parsed.Value);
+            Assert.Equal<byte[]>(parsed.Value, buffer);
+        }
+
+        [Fact]
+        public void ParseSimpleNumber()
+        {
+            var buffer = new byte[] { 0x92, 0x10, 0x0, 0x0 };
+            var format = "u32";
+            var parsed = RegisterParser.ParseBuffer(format, buffer);
+
+            Assert.True(parsed.IsSingleValue);
+            Assert.IsType<UInt32>(parsed.Value);
+            Assert.Equal<UInt32>(4242, parsed.GetValue<UInt32>());
+        }
+
+        [Fact]
+        public void ParseTwoSimpleNumbers()
+        {
+            var buffer = new byte[] { 0x92, 0x10, 0x0, 0x0, 0xCF, 0x7, 0x0, 0x0 };
+            var format = "u32 u32";
+            var parsed = RegisterParser.ParseBuffer(format, buffer);
+
+            Assert.True(2 == parsed.Values.Length);
+            Assert.True(4242 == parsed.GetValue<uint>(0));
+            Assert.True(1999 == parsed.GetValue<uint>(1));
+        }
+
+        [Fact]
+        public void ParseFloat()
+        {
+            var buffer = new byte[] { 0x7F, 0xFF };
+            var format = "u8.8";
+            var parsed = RegisterParser.ParseBuffer(format, buffer);
+
+            Assert.True(parsed.IsSingleValue);
+            Assert.IsType<float>(parsed.Value);
+            Assert.Equal<float>(255.4961f, parsed.Value);
+        }
+
+        [Fact]
+        public void ParseString()
+        {
+            var buffer = new byte[] { 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100 };
+            var format = "s";
+            var parsed = RegisterParser.ParseBuffer(format, buffer);
+
+            Assert.True(parsed.IsSingleValue);
+            Assert.IsType<string>(parsed.Value);
+            Assert.Equal("Hello World", parsed.Value);
+        }
+
+        [Fact]
+        public void ParseTerminatedString()
+        {
+            var buffer = new byte[] { 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 0, 0xFF };
+            var format = "z u8";
+            var parsed = RegisterParser.ParseBuffer(format, buffer);
+
+            Assert.False(parsed.IsSingleValue);
+            Assert.IsType<string>(parsed.Values[0]);
+            Assert.IsType<byte>(parsed.Values[1]);
+            Assert.Equal("Hello World", parsed.GetValue<string>(0));
+            Assert.Equal<byte>(255, parsed.GetValue<byte>(1));
+        }
+
+        [Fact]
+        public void ParseExhaustingArray()
+        {
+            var buffer = new byte[] { 0x7F, 0xCD, 0xAB, 0x34, 0x12, 0x78, 0x56 };
+            var format = "u8 u16[]";
+            var parsed = RegisterParser.ParseBuffer(format, buffer);
+
+            Assert.False(parsed.IsSingleValue);
+            Assert.IsType<byte>(parsed.Values[0]);
+            Assert.Equal<byte>(127, parsed.GetValue<byte>(0));
+
+            Assert.IsType<ushort>(parsed.Values[1]);
+            Assert.Equal<ushort>(43981, parsed.GetValue<ushort>(1));
+            Assert.Equal<ushort>(4660, parsed.GetValue<ushort>(2));
+            Assert.Equal<ushort>(22136, parsed.GetValue<ushort>(3));
+        }
+
+        [Fact]
+        public void ParseSizeArray()
+        {
+            var buffer = new byte[] { 0x7F, 0xCD, 0xAB, 0x34, 0x12, 0xFF };
+            var format = "u8 u16[2] u8";
+            var parsed = RegisterParser.ParseBuffer(format, buffer);
+
+            Assert.False(parsed.IsSingleValue);
+            Assert.IsType<byte>(parsed.Values[0]);
+            Assert.Equal<byte>(127, parsed.GetValue<byte>(0));
+
+            Assert.IsType<object[]>(parsed.Values[1]);
+            Assert.IsType<ushort>(parsed.Values[1][0]);
+            Assert.Equal<ushort>(43981, (ushort) parsed.GetValue<object[]>(1)[0]);
+            Assert.Equal<ushort>(4660, (ushort) parsed.GetValue<object[]>(1)[1]);
+
+            Assert.IsType<byte>(parsed.Values[2]);
+            Assert.Equal<byte>(255, parsed.GetValue<byte>(2));
+        }
+
+        [Theory]
+        [InlineData(new byte[] {0xFF, 0xFF}, "u8 u8", new object[] { 255, 255 })]
+        [InlineData(
+            new byte[] { 0x7F, 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100 }, 
+            "u8 s", 
+            new object[] { 127, "Hello World" })]
+        [InlineData(
+            new byte[] { 0x7F, 0xFF, 0x15, 0x1, 0x3B, 0x1, 0xEF, 0xEF },
+            "u8 u8 r: u16",
+            new object[] { 127, 255, 277, 315, 61423 })]
+        [InlineData(
+            new byte[] { 0x7F, 0xFF, 72, 101, 108, 108, 111, 0, 1, 87, 111, 114, 108, 100, 0 },
+            "u8 r: u8 z",
+            new object[] { 127, 255, "Hello", 1, "World"})]
+        public void ParseArbitrary(byte[] buffer, string format, dynamic[] expectedValues)
+        {
+            var parsed = RegisterParser.ParseBuffer(format, buffer);
+            for(int i = 0; i < expectedValues.Length; i++)
+            {
+                Assert.True(parsed.Values[i] == expectedValues[i]);
+            }
+        }
+    }
+}
---
+++ Jacdac/JDRegister.cs
@@ -0,0 +1,68 @@
+﻿using Jacdac.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jacdac
+{
+    public class JDRegister<T> : JDRegister
+    {
+        private const int REFRESH_TIMEOUT_MS = 500;
+
+        private T cachedValue;
+
+        public T Value => GetValue().GetAwaiter().GetResult();
+
+        public DateTime LastFetched { get; private set; }
+
+        public JDRegister(ushort address, RegisterFlags flags, JDService service) : base(address, flags, service)
+        {
+        }
+
+        public async Task<T> GetValue(bool forceRefresh = false)
+        {
+            var requiresRefresh = forceRefresh;
+
+            if ((DateTime.Now - LastFetched).TotalMilliseconds > REFRESH_TIMEOUT_MS)
+                requiresRefresh = true;
+
+            if (requiresRefresh)
+                cachedValue = await FetchValue();
+
+            return cachedValue;
+        } 
+
+        private async Task<T> FetchValue()
+        {
+            var resultBuffer = await Service.ReadRegister(Address);
+            // Parse the result buffer here
+            return default(T);
+        }
+    }
+
+    public class JDRegister
+    {
+        public JDRegister(ushort address, RegisterFlags flags, JDService service)
+        {
+            Address = address;
+            Service = service;
+            Flags = flags;
+        }
+
+        public ushort Address { get; }
+        public JDService Service { get; }
+
+        public RegisterFlags Flags { get; }
+    }
+
+    [Flags]
+    public enum RegisterFlags
+    {
+        Read,
+        Write,
+        Const,
+        Client
+    }
+}
