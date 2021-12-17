1f24b9fc0aaa0c21fa3dc85b0e88b7f18516af5b
pelikhan <jhalleux@microsoft.com>
2021-12-17 11:57:08 -0800

    add hf2 project

---
+++ Jacdac.HF2/HF2Transport.cs
@@ -0,0 +1,14 @@
+﻿namespace Jacdac.Transports
+{
+    public abstract class HF2Transport : Jacdac.Transport
+    {
+        protected HF2Transport()
+            : base()
+        {
+
+        }
+
+        public abstract Task SendData(byte[] buffer);
+    }
+
+}
