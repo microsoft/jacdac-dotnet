a4d6edd4911b68e4600d2fc3a927576180022202
pelikhan <jhalleux@microsoft.com>
2022-01-06 12:11:49 -0800

    don't generate changelog

--- release.config.js
+++ release.config.js
@@ -11,15 +11,10 @@ module.exports = {
         },
         // Verifies the conditions for the plugins used below
         // For example verifying a GITHUB_TOKEN environment variable has been provided
-        '@semantic-release/changelog',
         '@semantic-release/git',
         '@semantic-release/github',
     ],
     publish: [
-        // https://github.com/semantic-release/changelog
-        // Set of semantic-release plugins for creating or updating a changelog file.
-        '@semantic-release/changelog',
-
         // https://github.com/semantic-release/git
         // Git plugin is need so the changelog file will be committed to the Git repository and available on subsequent builds in order to be updated.
         '@semantic-release/git',
