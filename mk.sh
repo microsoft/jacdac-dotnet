9f9582171e7c5986dab436beebda8e003ca5cd3b
pelikhan <jhalleux@microsoft.com>
2022-01-06 15:45:43 -0800

    use script

---
+++ mk.sh
@@ -0,0 +1,6 @@
+dotnet build ./Jacdac/Jacdac.csproj -c Debug
+dotnet build ./Jacdac.NET/Jacdac.NET.csproj -c Release
+dotnet build ./Jacdac.NET.Clients/Jacdac.NET.Clients.csproj -c Release
+dotnet build ./Jacdac.NET.Transports.Hf2/Jacdac.NET.Transports.Hf2.csproj -c Release
+dotnet build ./Jacdac.NET.Transports.Spi/Jacdac.NET.Transports.Spi.csproj -c Release
+dotnet build ./Jacdac.NET.Transports.WebSockets/Jacdac.NET.Transports.WebSockets.csproj -c Release
