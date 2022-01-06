b7f0adde1a0e6dd6839f7eff8046b7803a9562c7
pelikhan <jhalleux@microsoft.com>
2022-01-06 13:28:47 -0800

    remove git publish

--- release.config.js
+++ release.config.js
@@ -15,8 +15,5 @@ module.exports = {
         '@semantic-release/github',
     ],
     publish: [
-        // https://github.com/semantic-release/git
-        // Git plugin is need so the changelog file will be committed to the Git repository and available on subsequent builds in order to be updated.
-        '@semantic-release/git',
     ],
 }
