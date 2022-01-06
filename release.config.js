55495d4707a47912e6a1eadae4352b17184ad367
pelikhan <jhalleux@microsoft.com>
2022-01-06 11:59:20 -0800

    attempt at semantic-release

---
+++ release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    verifyConditions: [
+        // Verifies a NUGET_TOKEN environment variable has been provided
+        () => {
+            if (!process.env.NUGET_TOKEN) {
+                throw new SemanticReleaseErrorc(
+                    'The environment variable NUGET_TOKEN is required.',
+                    'ENOAPMTOKEN',
+                )
+            }
+        },
+        // Verifies the conditions for the plugins used below
+        // For example verifying a GITHUB_TOKEN environment variable has been provided
+        '@semantic-release/changelog',
+        '@semantic-release/git',
+        '@semantic-release/github',
+    ],
+    publish: [
+        // https://github.com/semantic-release/changelog
+        // Set of semantic-release plugins for creating or updating a changelog file.
+        '@semantic-release/changelog',
+
+        // https://github.com/semantic-release/git
+        // Git plugin is need so the changelog file will be committed to the Git repository and available on subsequent builds in order to be updated.
+        '@semantic-release/git',
+    ],
+}
