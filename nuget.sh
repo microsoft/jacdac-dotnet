d1db2baa39e1cdbecff138f8b7e67f30c634dfcd
pelikhan <jhalleux@microsoft.com>
2022-01-06 22:57:25 -0800

    fix: inline nuget feed url

--- nuget.sh
+++ nuget.sh
@@ -1,4 +1,4 @@
 dotnet clean Jacdac.sln
 dotnet build Jacdac.sln -c Release
 dotnet pack Jacdac.sln -c Release -o packages
-dotnet nuget push packages/*.nupkg -k $NUGET_TOKEN -s $NUGET_PUSH_URL
+dotnet nuget push packages/*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
