5c2673fb4760f01282d53bcf627d1eb646d0645d
pelikhan <jhalleux@microsoft.com>
2022-01-11 14:01:32 -0800

    attempt at fixing nuget.sh

--- nuget.sh
+++ nuget.sh
@@ -1,6 +1,6 @@
-msbuild -t:clean Jacdac.sln
-msbuild -t:restore Jacdac.sln
-msbuild Jacdac.sln -p:Configuration=Release
+dotnet clean Jacdac.sln
+dotnet build Jacdac.sln -c Release
+dotnet pack Jacdac.sln -c Release -o packages
 dotnet nuget push ./bin/Release/packages/Jacdac.NET.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
 dotnet nuget push ./bin/Release/packages/Jacdac.DevTools*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
 dotnet nuget push ./bin/Release/Jacdac.TinyCLR.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
