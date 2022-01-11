09e1eb14ed7cbd735444835ecbb4502921bb4d9d
pelikhan <jhalleux@microsoft.com>
2022-01-11 07:06:24 -0800

    updated output folder

--- nuget.sh
+++ nuget.sh
@@ -1,3 +1,3 @@
-dotnet nuget push packages/Jacdac.NET.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
-dotnet nuget push packages/Jacdac.TinyCLR.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
-dotnet nuget push packages/Jacdac.Nano.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
+dotnet nuget push ./bin/Release/packages/Jacdac.NET.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
+dotnet nuget push ./bin/Release/Jacdac.TinyCLR.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
+dotnet nuget push ./bin/Release/Jacdac.Nano.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
