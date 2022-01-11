1af870354ada8e2e4284247542aaeb76f387eb62
pelikhan <jhalleux@microsoft.com>
2022-01-11 07:02:11 -0800

    build release

--- nuget.sh
+++ nuget.sh
@@ -1,4 +1,3 @@
-dotnet clean Jacdac.sln
-dotnet build Jacdac.sln -c Release
-dotnet pack Jacdac.sln -c Release -o packages
 dotnet nuget push packages/Jacdac.NET.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
+dotnet nuget push packages/Jacdac.TinyCLR.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
+dotnet nuget push packages/Jacdac.Nano.*.nupkg -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
