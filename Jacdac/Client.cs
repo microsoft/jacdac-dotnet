07c2e6ab234da805ba6307bab7b2aa2e574ba363
pelikhan <jhalleux@microsoft.com>
2022-01-11 17:24:09 -0800

    patch: C02 sample

--- Jacdac/Client.cs
+++ Jacdac/Client.cs
@@ -320,6 +320,7 @@ namespace Jacdac
             var ev = this.ReadingChanged;
             if (ev != null)
                 this.ReadingChanged?.Invoke(this, EventArgs.Empty);
+            this.RaiseChanged();
         }
 
         /// <summary>
---
+++ Jacdac/Samples/CO2Alarm.cs
@@ -0,0 +1,36 @@
+﻿using Jacdac.Clients;
+using System;
+using System.Threading;
+
+namespace Jacdac.Samples
+{
+    internal class CO2Alarm : ISample
+    {
+        public void Run(JDBus bus)
+        {
+            // DISCLAIMER: This is a sample and should not be used as reference to build
+            // any kind of device.
+            var co2 = new ECO2Client(bus, "co2");
+            var buzzer = new BuzzerClient(bus, "buzzer");
+            var led = new LedClient(bus, "led");
+            co2.ReadingChanged += (s, e) =>
+            {
+                var value = co2.ECO2;
+                // too much CO2
+                if (value > 1000)
+                {
+                    Console.WriteLine("too much CO2, ventilate!");
+                    // turn lights on
+                    led.SetColor(0xff0000);
+                    // sound alarm
+                    buzzer.PlayNote(800, 1, 500);
+                }
+                else if (value < 900)
+                {
+                    Console.WriteLine("CO2 ok");
+                    led.SetColor(0);
+                }
+            };
+        }
+    }
+}
