ef98bd6e49046a06ebd1aef33d58113d21bf0c9f
pelikhan <jhalleux@microsoft.com>
2022-01-12 15:55:41 -0800

    more attempts

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
-msbuild -t:clean Jacdac.sln
-msbuild -t:build -p:Configuration=Release Jacdac.sln
+msbuild.exe -t:clean Jacdac.sln
+msbuild.exe -t:build -p:Configuration=Release Jacdac.sln
 dotnet pack Jacdac.sln -c Release -o newpackages
 nuget.exe pack ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -Prop Configuration=Release -OutputDirectory ./newpackages
