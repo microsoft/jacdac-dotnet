31cc0a3f146879fc0f973eceddff7f701fc7903b
pelikhan <jhalleux@microsoft.com>
2022-01-11 16:40:47 -0800

    added gamepad mouse sample

---
+++ Jacdac/Samples/GamepadMouse.cs
@@ -0,0 +1,22 @@
+﻿using Jacdac.Clients;
+using System;
+using System.Threading;
+
+namespace Jacdac.Samples
+{
+    internal class GamepadMouse : ISample
+    {
+        public void Run(JDBus bus)
+        {
+            var gamepad = new GamepadClient(bus, "gamepad");
+            var mouse = new HidMouseClient(bus, "mouse");
+            gamepad.ReadingChanged += (s, e) =>
+            {
+                var dx = (int)(gamepad.X * 100);
+                var dy = (int)(gamepad.Y * 100);
+                if (Math.Abs(dx) > 20 || Math.Abs(dy) > 20)
+                    mouse.Move(dx / 4, dy / 4, 0);
+            };
+        }
+    }
+}
