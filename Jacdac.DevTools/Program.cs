4d112c591a24b84b9c96a12e1f7723908e0b63d5
pelikhan <jhalleux@microsoft.com>
2022-01-13 14:52:38 -0800

    patch: implement websocket proxy as transport

--- Jacdac.DevTools/Program.cs
+++ Jacdac.DevTools/Program.cs
@@ -6,182 +6,233 @@ using System.Net;
 using System.Net.Sockets;
 using System.Net.WebSockets;
 
-var internet = args.Any(arg => arg == "--internet");
-var spi = args.Any(arg => arg == "--spi");
-var stats = args.Any(arg => arg == "--stats");
-var host = internet ? "*" : "localhost";
-var port = 8081;
-var url = $"http://{host}:{port}";
-
-Console.WriteLine("Jacdac DevTools (.NET)");
-Console.WriteLine("");
-Console.WriteLine("  --spi       enable SPI transport");
-Console.WriteLine("  --internet  bind all network interfaces");
-Console.WriteLine("  --stats     show various stats");
-Console.WriteLine("");
-Console.WriteLine($"   dashboard: {url}");
-Console.WriteLine($"   websocket: ws://{host}:{port}");
-if (internet)
+namespace Jacdac.DevTools
 {
-    Console.WriteLine("WARNING: all network interfaces bound");
-    var server = Dns.GetHostName();
-    var heserver = Dns.GetHostEntry(server);
-    foreach (var ip in heserver.AddressList)
-        Console.WriteLine($"  {ip}");
-}
+    public class ProxyTransport : Transport
+    {
+        public delegate void SendFrameHandler(byte[] data);
+        private SendFrameHandler sendFrame;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-if (app.Environment.IsDevelopment())
-    app.UseDeveloperExceptionPage();
-
-// better concurrent data structure?
-var clients = new List<WebSocket>();
-void SendFrame(WebSocket[] cs, byte[] frame)
-{
-    if (cs.Length == 0) return;
-    Task.WaitAll(
-        cs
-        .Select(async (client) =>
+        public ProxyTransport(SendFrameHandler sendFrame)
+            : base("devtools")
         {
-            try
+            this.sendFrame = sendFrame;
+        }
+
+        public override event FrameEventHandler? FrameReceived;
+        public override event TransportErrorReceivedEvent? ErrorReceived;
+
+        public void RaiseFrameReceived(byte[] frame)
+        {
+            this.FrameReceived?.Invoke(this, frame);
+        }
+
+        public override void SendFrame(byte[] data)
+        {
+            this.sendFrame(data);
+        }
+
+        protected override void InternalConnect()
+        {
+            this.SetConnectionState(ConnectionState.Connected);
+        }
+
+        protected override void InternalDisconnect()
+        {
+            this.SetConnectionState(ConnectionState.Disconnected);
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            AsyncMain(args).Wait();
+        }
+
+        static async Task AsyncMain(string[] args)
+        {
+            var internet = args.Any(arg => arg == "--internet");
+            var spi = args.Any(arg => arg == "--spi");
+            var stats = args.Any(arg => arg == "--stats");
+            var host = internet ? "*" : "localhost";
+            var port = 8081;
+            var url = $"http://{host}:{port}";
+
+            Console.WriteLine("Jacdac DevTools (.NET)");
+            Console.WriteLine("");
+            Console.WriteLine("  --spi       enable SPI transport");
+            Console.WriteLine("  --internet  bind all network interfaces");
+            Console.WriteLine("  --stats     show various stats");
+            Console.WriteLine("");
+            Console.WriteLine($"   dashboard: {url}");
+            Console.WriteLine($"   websocket: ws://{host}:{port}");
+            if (internet)
             {
-                await client.SendAsync(frame, WebSocketMessageType.Binary, true, CancellationToken.None);
+                Console.WriteLine("WARNING: all network interfaces bound");
+                var server = Dns.GetHostName();
+                var heserver = Dns.GetHostEntry(server);
+                foreach (var ip in heserver.AddressList)
+                    Console.WriteLine($"  {ip}");
             }
-            catch
+
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            // better concurrent data structure?
+            var clients = new List<WebSocket>();
+            void SendFrame(WebSocket[] cs, byte[] frame)
+            {
+                if (cs.Length == 0) return;
+                Task.WaitAll(
+                    cs
+                    .Select(async (client) =>
+                    {
+                        try
+                        {
+                            await client.SendAsync(frame, WebSocketMessageType.Binary, true, CancellationToken.None);
+                        }
+                        catch
+                        {
+                            lock (clients)
+                                clients.Remove(client);
+                            try
+                            {
+                                client?.Dispose();
+                            }
+                            catch { }
+                        }
+                    }).ToArray());
+            }
+
+            ProxyTransport? proxyTransport = null;
+            if (spi)
+            {
+                Console.WriteLine("starting Jacdac bus...");
+                var spiTransport = SpiTransport.Create();
+                proxyTransport = new ProxyTransport(frame =>
+                {
+                    WebSocket[] cs;
+                    lock (clients)
+                        cs = clients.ToArray();
+                    SendFrame(cs, frame);
+                });
+                var bus = new JDBus(spiTransport, new JDBusOptions
+                {
+                    IsClient = false,
+                    IsInfrastructure = true,
+                    DisableUniqueBrain = true,
+                    DisableRoleManager = true,
+                    DisableLogger = true,
+                });
+                bus.AddTransport(proxyTransport);
+                bus.DeviceConnected += (s, e) =>
+                {
+                    Console.WriteLine($"device connected: {e.Device}");
+                    e.Device.Restarted += (s2, e2) => Console.WriteLine($"device restarted: {e.Device}");
+                };
+                bus.DeviceDisconnected += (sender, device) => Console.WriteLine($"device disconnected: {device.Device}");
+                if (stats)
+                {
+                    new Timer(state =>
+                    {
+                        Console.Write(bus);
+                        Console.WriteLine(spiTransport);
+                    }, null, 0, 30000);
+                }
+            }
+
+            // download proxy code
+            var resp = await new HttpClient().GetAsync("https://microsoft.github.io/jacdac-docs/devtools/proxy");
+            resp.EnsureSuccessStatusCode();
+            var proxySource = await resp.Content.ReadAsStringAsync();
+
+            app.Lifetime.ApplicationStopping.Register(() =>
             {
                 lock (clients)
-                    clients.Remove(client);
-                try
                 {
-                    client?.Dispose();
-                }
-                catch { }
-            }
-        }).ToArray());
-}
-
-JDBus? bus = null;
-if (spi)
-{
-    Console.WriteLine("starting Jacdac bus...");
-    var spiTransport = SpiTransport.Create();
-    bus = new JDBus(spiTransport, new JDBusOptions
-    {
-        IsClient = false,
-        IsInfrastructure = true,
-        DisableUniqueBrain = true,
-        DisableRoleManager = true,
-        DisableLogger = true,
-    });
-    bus.DeviceConnected += (s, e) =>
-    {
-        Console.WriteLine($"device connected: {e.Device}");
-        e.Device.Restarted += (s2, e2) => Console.WriteLine($"device restarted: {e.Device}");
-    };
-    bus.DeviceDisconnected += (sender, device) => Console.WriteLine($"device disconnected: {device.Device}");
-    bus.FrameSent += (sender, frame) =>
-    {
-        WebSocket[] cs;
-        lock (clients)
-            cs = clients.ToArray();
-        SendFrame(cs, frame);
-    };
-    if (stats)
-    {
-        new Timer(state =>
-        {
-            Console.Write(bus);
-            Console.WriteLine(spiTransport);
-        }, null, 0, 30000);
-    }
-}
-
-// download proxy code
-var resp = await new HttpClient().GetAsync("https://microsoft.github.io/jacdac-docs/devtools/proxy");
-resp.EnsureSuccessStatusCode();
-var proxySource = await resp.Content.ReadAsStringAsync();
-
-app.Lifetime.ApplicationStopping.Register(() =>
-{
-    lock (clients)
-    {
-        foreach (var client in clients)
-        {
-            try
-            {
-                client.Dispose();
-            }
-            catch { }
-        }
-        clients.Clear();
-    }
-});
-app.UseWebSockets();
-app.Use(async (context, next) =>
-{
-    if (context.Request.Path == "/" && context.WebSockets.IsWebSocketRequest)
-    {
-        var ws = await context.WebSockets.AcceptWebSocketAsync();
-        lock (clients)
-        {
-            clients.Add(ws);
-            Console.WriteLine($"clients: {clients.Count} connected");
-        }
-        var proxy = async () =>
-            {
-                try
-                {
-                    var buffer = new byte[512];
-                    while (ws.State == WebSocketState.Open)
+                    foreach (var client in clients)
                     {
-                        // grab frame
-                        var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                        var frame = buffer.Take(result.Count).ToArray();
-                        // dispatch to bus
-                        bus?.ProcessFrame(null, frame);
-                        // dispatch to other clients
-                        WebSocket[] cs;
-                        lock (clients)
-                            cs = clients.Where(client => client != ws).ToArray();
-                        SendFrame(cs, frame);
+                        try
+                        {
+                            client.Dispose();
+                        }
+                        catch { }
                     }
+                    clients.Clear();
                 }
-                catch (SocketException)
+            });
+            app.UseWebSockets();
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/" && context.WebSockets.IsWebSocketRequest)
                 {
-
-                }
-                finally
-                {
-                    // web socket closed, clean
+                    var ws = await context.WebSockets.AcceptWebSocketAsync();
                     lock (clients)
                     {
-                        clients.Remove(ws);
+                        clients.Add(ws);
                         Console.WriteLine($"clients: {clients.Count} connected");
                     }
-                    try
-                    {
+                    var proxy = async () =>
+                        {
+                            try
+                            {
+                                var buffer = new byte[512];
+                                while (ws.State == WebSocketState.Open)
+                                {
+                                    // grab frame
+                                    var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                                    var frame = buffer.Take(result.Count).ToArray();
+                                    // dispatch to bus
+                                    proxyTransport?.RaiseFrameReceived(frame);
+                                    // dispatch to other clients
+                                    WebSocket[] cs;
+                                    lock (clients)
+                                        cs = clients.Where(client => client != ws).ToArray();
+                                    SendFrame(cs, frame);
+                                }
+                            }
+                            catch (SocketException)
+                            {
 
-                    }
-                    catch
-                    {
-                        ws.Dispose();
-                    }
+                            }
+                            finally
+                            {
+                                // web socket closed, clean
+                                lock (clients)
+                                {
+                                    clients.Remove(ws);
+                                    Console.WriteLine($"clients: {clients.Count} connected");
+                                }
+                                try
+                                {
+
+                                }
+                                catch
+                                {
+                                    ws.Dispose();
+                                }
+                            }
+                        };
+                    await proxy();
                 }
-            };
-        await proxy();
-    }
-    else if (context.Request.Path == "/")
-    {
-        context.Response.Headers.ContentType = "text/html";
-        context.Response.Headers.CacheControl = "no-cache";
-        await context.Response.WriteAsync(proxySource);
-        await context.Response.CompleteAsync();
-    }
-    else
-    {
-        await next();
-    }
+                else if (context.Request.Path == "/")
+                {
+                    context.Response.Headers.ContentType = "text/html";
+                    context.Response.Headers.CacheControl = "no-cache";
+                    await context.Response.WriteAsync(proxySource);
+                    await context.Response.CompleteAsync();
+                }
+                else
+                {
+                    await next();
+                }
 
-});
-app.Run(url);
+            });
+            app.Run(url);
+
+        }
+    }
+}
