066d75ad39935009130875a6908e5cf550269fdb
pelikhan <jhalleux@microsoft.com>
2022-01-14 14:11:18 -0800

    escaping sh

--- pack.sh
+++ pack.sh
@@ -1,5 +1,5 @@
 msbuild.exe -t:clean Jacdac.sln
+nuget.exe restore ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -OutputDirectory ./Jacdac.TinyCLR/packages
 msbuild.exe -t:build -p:Configuration=Release Jacdac.sln
-nuget.exe restore .\Jacdac.TinyCLR\Jacdac.TinyCLR.csproj -OutputDirectory .\Jacdac.TinyCLR\packages
 dotnet pack Jacdac.sln -c Release -o newpackages
 nuget.exe pack ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -Prop Configuration=Release -OutputDirectory ./newpackages
