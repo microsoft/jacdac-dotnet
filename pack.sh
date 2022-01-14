ecb82ec5031b1f7e72ff87bb6134c3f216330617
pelikhan <jhalleux@microsoft.com>
2022-01-14 14:24:23 -0800

    increase logging

--- pack.sh
+++ pack.sh
@@ -1,5 +1,5 @@
 msbuild.exe -t:clean Jacdac.sln
-nuget.exe restore ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -OutputDirectory ./Jacdac.TinyCLR/packages
+nuget.exe restore ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -OutputDirectory ./Jacdac.TinyCLR/packages -Verbosity detailed
 msbuild.exe -t:build -p:Configuration=Release Jacdac.sln
 dotnet pack Jacdac.sln -c Release -o newpackages
-nuget.exe pack ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -Prop Configuration=Release -OutputDirectory ./newpackages
+nuget.exe pack ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -Prop Configuration=Release -OutputDirectory ./newpackages -Verbosity detailed
