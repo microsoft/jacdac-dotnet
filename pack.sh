4bf505b349a8d634bcff463d09db55978e65cea1
pelikhan <jhalleux@microsoft.com>
2022-01-18 11:01:32 -0800

    build more tinyclr assemblies

--- pack.sh
+++ pack.sh
@@ -1,3 +1,6 @@
+# clean
+msbuild.exe -t:clean Jacdac.sln
+
 # Jacdac.TinyCLR
 cd Jacdac.TinyCLR
 msbuild.exe -t:clean Jacdac.TinyCLR.csproj
@@ -5,8 +8,27 @@ nuget.exe restore Jacdac.TinyCLR.csproj -OutputDirectory packages -Verbosity det
 ls -R
 msbuild.exe -t:build -p:Configuration=Release Jacdac.TinyCLR.csproj -v:d
 nuget.exe pack Jacdac.TinyCLR.csproj -Prop Configuration=Release -OutputDirectory ../newpackages -Verbosity detailed -PackagesDirectory ./packages
+cd ..
 
-#msbuild.exe -t:clean Jacdac.sln
-#nuget.exe restore Jacdac.sln
-#msbuild.exe -t:build -p:Configuration=Release Jacdac.sln
-#dotnet pack Jacdac.sln -c Release -o newpackages
+# Jacdac.TinyCLR.Clients
+cd Jacdac.TinyCLR.Clients
+msbuild.exe -t:clean Jacdac.TinyCLR.Clients.csproj
+nuget.exe restore Jacdac.TinyCLR.Clients.csproj -OutputDirectory packages -Verbosity detailed
+ls -R
+msbuild.exe -t:build -p:Configuration=Release Jacdac.TinyCLR.Clients.csproj -v:d
+nuget.exe pack Jacdac.TinyCLR.Clients.csproj -Prop Configuration=Release -OutputDirectory ../newpackages -Verbosity detailed -PackagesDirectory ./packages
+cd ..
+
+# Jacdac.TinyCLR.Storage
+cd Jacdac.TinyCLR.Storage
+msbuild.exe -t:clean Jacdac.TinyCLR.Storage.csproj
+nuget.exe restore Jacdac.TinyCLR.Storage.csproj -OutputDirectory packages -Verbosity detailed
+ls -R
+msbuild.exe -t:build -p:Configuration=Release Jacdac.TinyCLR.Storage.csproj -v:d
+nuget.exe pack Jacdac.TinyCLR.Storage.csproj -Prop Configuration=Release -OutputDirectory ../newpackages -Verbosity detailed -PackagesDirectory ./packages
+cd ..
+
+# .NET assemblies
+nuget.exe restore Jacdac.sln
+msbuild.exe -t:build -p:Configuration=Release Jacdac.sln
+dotnet pack Jacdac.sln -c Release -o newpackages
