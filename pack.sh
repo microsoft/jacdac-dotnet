2054b9080828f332d025076ab53f48d16a12fa4d
pelikhan <jhalleux@microsoft.com>
2022-01-18 15:41:30 -0800

    fix file names

--- pack.sh
+++ pack.sh
@@ -27,10 +27,10 @@ msbuild.exe -t:build -p:Configuration=Release Jacdac.sln
 dotnet pack Jacdac.DevTools/Jacdac.DevTools.csproj -c Release -o newpackages
 dotnet pack Jacdac.NET/Jacdac.NET.csproj -c Release -o newpackages
 dotnet pack Jacdac.NET.Clients/Jacdac.NET.Clients.csproj -c Release -o newpackages
-dotnet pack Jacdac.NET.Transports.Hf2/Jacdac.Transports.Hf2.csproj -c Release -o newpackages
-dotnet pack Jacdac.NET.Transports.Spi/Jacdac.Transports.Spi.csproj -c Release -o newpackages
-dotnet pack Jacdac.NET.Transports.WebSockets/Jacdac.Transports.WebSockets.csproj -c Release -o newpackages
-dotnet pack Jacdac.NET.Servers.SoundPlayer/Jacdac.Servers.SoundPlayer.csproj -c Release -o newpackages
+dotnet pack Jacdac.NET.Transports.Hf2/Jacdac.NET.Transports.Hf2.csproj -c Release -o newpackages
+dotnet pack Jacdac.NET.Transports.Spi/Jacdac.NET.Transports.Spi.csproj -c Release -o newpackages
+dotnet pack Jacdac.NET.Transports.WebSockets/Jacdac.NET.Transports.WebSockets.csproj -c Release -o newpackages
+dotnet pack Jacdac.NET.Servers.SoundPlayer/Jacdac.NET.Servers.SoundPlayer.csproj -c Release -o newpackages
 
 # let's see  who got built
 ls newpackages
