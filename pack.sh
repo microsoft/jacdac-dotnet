daf7c74c594622b13905dc4d931e171741f9fef2
pelikhan <jhalleux@microsoft.com>
2022-01-18 10:26:18 -0800

    try build before pack

--- pack.sh
+++ pack.sh
@@ -1,7 +1,8 @@
 msbuild.exe -t:clean Jacdac.sln
 nuget.exe restore ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -OutputDirectory ./Jacdac.TinyCLR/packages -Verbosity detailed
 ls -R
+msbuild.exe -t:build -p:Configuration=Release ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj
 nuget.exe pack ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -Prop Configuration=Release -OutputDirectory ./newpackages -Verbosity detailed
-nuget.exe restore Jacdac.sln
-msbuild.exe -t:build -p:Configuration=Release Jacdac.sln
-dotnet pack Jacdac.sln -c Release -o newpackages
+#nuget.exe restore Jacdac.sln
+#msbuild.exe -t:build -p:Configuration=Release Jacdac.sln
+#dotnet pack Jacdac.sln -c Release -o newpackages
