dab2900ce5871184abf575e35389e008f3199527
pelikhan <jhalleux@microsoft.com>
2022-01-18 10:21:15 -0800

    move Jacdac.TinyCLR up

--- pack.sh
+++ pack.sh
@@ -1,6 +1,7 @@
 msbuild.exe -t:clean Jacdac.sln
 nuget.exe restore ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -OutputDirectory ./Jacdac.TinyCLR/packages -Verbosity detailed
 ls -R
+nuget.exe pack ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -Prop Configuration=Release -OutputDirectory ./newpackages -Verbosity detailed
+nuget.exe restore Jacdac.sln
 msbuild.exe -t:build -p:Configuration=Release Jacdac.sln
 dotnet pack Jacdac.sln -c Release -o newpackages
-nuget.exe pack ./Jacdac.TinyCLR/Jacdac.TinyCLR.csproj -Prop Configuration=Release -OutputDirectory ./newpackages -Verbosity detailed
