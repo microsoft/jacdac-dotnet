80aea5f159ce76ef730ae9cd7b2fa9d46bacafad
pelikhan <jhalleux@microsoft.com>
2022-05-04 15:49:19 -0700

    updated specs

---
+++ Jacdac/Constants/DualmotorsConstants.g.cs
@@ -0,0 +1,81 @@
+namespace Jacdac {
+    public static partial class ServiceClasses
+    {
+        public const uint DualMotors = 0x1529d537;
+    }
+    public enum DualMotorsReg : ushort {
+        /// <summary>
+        /// Relative speed of the motors. Use positive/negative values to run the motor forwards and backwards.
+        /// A speed of ``0`` while ``enabled`` acts as brake.
+        ///
+        /// ```
+        /// const [left, right] = jdunpack<[number, number]>(buf, "i1.15 i1.15")
+        /// ```
+        /// </summary>
+        Speed = 0x2,
+
+        /// <summary>
+        /// Read-write bool (uint8_t). Turn the power to the motors on/off.
+        ///
+        /// ```
+        /// const [enabled] = jdunpack<[number]>(buf, "u8")
+        /// ```
+        /// </summary>
+        Enabled = 0x1,
+
+        /// <summary>
+        /// Constant kg/cm u16.16 (uint32_t). Torque required to produce the rated power of an each electrical motor at load speed.
+        ///
+        /// ```
+        /// const [loadTorque] = jdunpack<[number]>(buf, "u16.16")
+        /// ```
+        /// </summary>
+        LoadTorque = 0x180,
+
+        /// <summary>
+        /// Constant rpm u16.16 (uint32_t). Revolutions per minute of the motor under full load.
+        ///
+        /// ```
+        /// const [loadRotationSpeed] = jdunpack<[number]>(buf, "u16.16")
+        /// ```
+        /// </summary>
+        LoadRotationSpeed = 0x181,
+
+        /// <summary>
+        /// Constant bool (uint8_t). Indicates if the motors can run backwards.
+        ///
+        /// ```
+        /// const [reversible] = jdunpack<[number]>(buf, "u8")
+        /// ```
+        /// </summary>
+        Reversible = 0x182,
+    }
+
+    public static class DualMotorsRegPack {
+        /// <summary>
+        /// Pack format for 'speed' register data.
+        /// </summary>
+        public const string Speed = "i1.15 i1.15";
+
+        /// <summary>
+        /// Pack format for 'enabled' register data.
+        /// </summary>
+        public const string Enabled = "u8";
+
+        /// <summary>
+        /// Pack format for 'load_torque' register data.
+        /// </summary>
+        public const string LoadTorque = "u16.16";
+
+        /// <summary>
+        /// Pack format for 'load_rotation_speed' register data.
+        /// </summary>
+        public const string LoadRotationSpeed = "u16.16";
+
+        /// <summary>
+        /// Pack format for 'reversible' register data.
+        /// </summary>
+        public const string Reversible = "u8";
+    }
+
+}
